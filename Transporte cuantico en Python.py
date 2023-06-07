eae27243fafd25beac61c570ac919c782571e5ce
enrik03 <enriquecasanova10@gmail.com>
2023-06-07 16:55:19 -0400

    #1

--- Transporte cuantico en Python.py
+++ Transporte cuantico en Python.py
@@ -1,7 +1,37 @@
 
 # Programa en python para transporte electronico 
 
+import math as m
+#import matplotlib as pt
+#import pandas as pd 
 
-a = True 
-if a == 1: 
-    print(True)
+# !Parameters
+# !--------------------------------------------------------------------------
+# real*8,parameter                       ::Pi       = 3.14159265358979323846d00
+# real*8,parameter                       ::Ech      = 1.0d0                           
+# real*8,parameter                       ::Hp       = 1.0d0                           
+# real*8,parameter                       ::Kb       = 8.6173324d-05                        
+# real*8,parameter                       ::CF       = 27.2116d00                        
+# real*8,parameter                       ::delta    = 1.0d-12
+# real*8,parameter                       ::test_E   = -5.50d00
+# contains 
+
+# First subroutine: input the initial data. will be stored in the input_data.dat file 
+
+ifileN = int(input()) 
+afileN = str 
+chart_int(ifileN,afileN) 
+open()
+if (stat != 0): 
+    print("Main: error opening ini file")
+
+FindTagA(10, "System =", Project)
+FindTagA(10, "Electrode =", Lead)
+FindTagI(10, "Number of levels =", NL)
+FindTagI(10, "Number of monomers =", NM)
+FindTagI(10, "Number of cicles =", NC)
+FindTagR(10, "DC bias voltage =", DC_volt)
+FindTagR(10, "Scape rate =", Gm)
+FindTagR(10, "Temperature =", Tp) 
+
+close()
